2110c0ceadd811efe058f5205e0745b90982225f
Evgeny <evgenij.baltais@gmail.com>
2020-07-07 20:49:37 +0300

    1

--- src/components/Selection.vue
+++ src/components/Selection.vue
@@ -4,6 +4,21 @@
 
 <VueRangeSlider v-model = "rangeValue" :min = "min" :max = "max" :enable-cross = "enableCross"></VueRangeSlider>
 
+<div class = "color-versions">
+
+    <div class = "color-variant custom-color choosen-color"></div>
+    <div class = "color-variant red-color"></div>
+    <div class = "color-variant white-color"></div>
+    <div class = "color-variant pink-color"></div>
+    <div class = "color-variant yellow-color"></div>
+    <div class = "color-variant orange-color"></div>
+    <div class = "color-variant purple-color"></div>
+    <div class = "color-variant green-color"></div>
+    <div class = "color-variant pastel-color"></div>
+    <div class = "color-variant bright-color"></div>
+    <div class = "color-variant random-color"></div>
+</div>
+
 </div>
 </template>
 
@@ -18,7 +33,7 @@ export default {
     name: 'Selection',
     data(){
         return {
-            rangeValue: [0, 100]
+            rangeValue: [0, 25000]
         }
     },
     components: {
@@ -26,7 +41,7 @@ export default {
     },
     created() {
         this.min = 0
-        this.max = 100
+        this.max = 25000
         this.speed = 1
         this.step = 0
         this.enableCross = false
